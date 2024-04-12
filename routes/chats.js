3863399a9b0a9f3a36aeb5c84c25e77c1d8a8c4e
Prem-Pandey <103317687+Prem-Pandey@users.noreply.github.com>
2024-04-12 11:18:02 +0530

    getting messages from database

--- routes/chats.js
+++ routes/chats.js
@@ -5,5 +5,5 @@ const userAuthentication = require("../middlewares/auth");
 const chatController = require("../controllers/chats");
 
 router.post("/chats",userAuthentication.authenticate,chatController.postAddChat)
-
+router.get("/chats",userAuthentication.authenticate,chatController.getChats)
 module.exports = router
